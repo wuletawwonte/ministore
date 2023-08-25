373008146dcd935ccff96109138d916e6e916d83
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-25 16:09:49 +0300

    Install devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
