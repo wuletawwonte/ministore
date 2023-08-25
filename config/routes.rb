8d4dbc71c23bed2172a6bf1592223fd04ffc4629
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-25 16:36:35 +0300

    Make devise sessions new path the root path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,13 @@
 
 Rails.application.routes.draw do
   devise_for :users
-  root 'pages#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_scope :user do
+    authenticated :user do
+      root 'pages#home', as: :authenticated_root
+    end
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
 end
