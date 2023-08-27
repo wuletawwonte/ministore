4e544b311f575993af89eb4edcff2b150b478845
Wuletaw Wonte <wuletawwonte@gmail.com>
2023-08-27 17:24:56 +0300

    Add find_for_oauth class method in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,17 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: [:microsoft_office365]
 
   has_many :inventory_items
+
+  def self.find_for_oauth(provider_data)
+    user = User.where(provider: provider_data.provider, uid: provider_data.uid).first
+
+    user ||= User.create(
+      uid: provider_data.uid,
+      provider: provider_data.provider,
+      email: provider_data.info.email,
+      password: Devise.friendly_token[0, 20]
+    )
+
+    user
+  end
 end
